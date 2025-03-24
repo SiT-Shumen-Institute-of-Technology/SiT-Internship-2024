d2a177e56ee4db712b2404c7736a2ac17b06e679
Simeon124 <simeon.vichev@pgmett.com>
2025-03-24 14:05:31 +0200

    Made an API controller with HTTP GET function

---
+++ SiT Access Control System/Web/SACS.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿namespace SACS.Web.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+    using SACS.Data.Models;
+    using SACS.Services.Data;
+
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeService employeeService;
+
+        public EmployeesController(IEmployeeService employeeService)
+        {
+            this.employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public List<Employee> GetEmployees()
+        {
+            return this.employeeService.GetAllEmployees();
+        }
+    }
+}
